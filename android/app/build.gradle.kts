d2f66e48b2517e55f43b4d768fab7bba8375e96c
Kaicess <princesstorilla023@gmail.com>
2026-05-07 23:07:14 +0800

    fixed build error by setting minSdk to 26

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,10 @@ android {
         applicationId = "com.example.leafloop"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        
+        // MANUALLY UPDATED FOR TFLITE COMPATIBILITY
+        minSdk = 26
+        
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
